0cb9050a68705ee26c530d0724300b6d8c6ae6f8
Matthew Hammond <mxh162@student.bham.ac.uk>
2023-11-04 22:11:35 +0000

    Simplify character parsing

--- src/scripts/Lexer/characters.js
+++ src/scripts/Lexer/characters.js
@@ -1,14 +1,36 @@
+/*
+    small     -> ascSmall | uniSmall | _
+    ascSmall  -> a | b | . . . | z
+    uniSmall  -> any Unicode lowercase letter
+
+    large     -> ascLarge | uniLarge
+    ascLarge  -> A | B | . . . | Z
+    uniLarge  -> any uppercase or titlecase Unicode letter
+
+    symbol    -> ascSymbol | uniSymbol⟨special⟩ | _ | " | ’i
+    ascSymbol -> ! | # | $ | % | & | * | + | . | / | < | = | > | ? | @
+               | \ | ˆ | | | - | ~ | :
+    uniSymbol -> any Unicode symbol or punctuation
+    
+    digit     -> ascDigit | uniDigit
+    ascDigit  -> 0 | 1 | . . . | 9
+    uniDigit  -> any Unicode decimal digit
+    octit     -> 0 | 1 | . . . | 7
+    hexit     -> digit | A | . . . | F | a | . . . | f
+*/
+
+
 let [dashes,  special,  small,  large,  symbol,  digit,  octit,  hexit,] = FunctionReference.n(
     "dashes","special","small","large","symbol","digit","octit","hexit",
 )
 
 dashes.set(all("--", many("-")));
 
-special.set(any("(", ")", ",", ";", "[", "]", "`", "{", "}"));
+special.set(any("(),;[]`{}"));
 
-small.set(any("_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
-large.set(any("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
-symbol.set(any("!", "#", "$", "%", "&", "*", "+", ".", "/", "\\", "<", "=", ">", "?", "@", "\\", "^", "|", "-", "~", ":", "]"));
-digit.set(any("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
-octit.set(any("0", "1", "2", "3", "4", "5", "6", "7"));
-hexit.set(any("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "a", "b", "c", "d", "e", "f"));
+small.set(any(..."_abcdefghijkmlnopqrstuvwxyz"));
+large.set(any(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+symbol.set(any(..."!#$%&*+./<=>?@\\^|-~:"));
+digit.set(any(..."0123456789"));
+octit.set(any(..."01234567"));
+hexit.set(any(..."0123456789ABCDEFabcdef"));
