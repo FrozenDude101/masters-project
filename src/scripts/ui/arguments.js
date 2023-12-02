c476061b7733f336eee84ca95f8d86b37fca5812
Matthew Hammond <mxh162@student.bham.ac.uk>
2023-12-02 18:33:50 +0000

    Fix inputs losing data when an argument was added

--- src/scripts/ui/arguments.js
+++ src/scripts/ui/arguments.js
@@ -1,14 +1,13 @@
 const ARGUMENT_TEMPLATE = 
-`<div class="argument" id="argument-{n}">
-    <select class="argument-type" id="argument-type-{n}">
-        <option>None</option>
-        <option>Integer</option>
-        <option>Float</option>
-        <option>String</option>
-        <option>Function</option>
-    </select>
-    <input class="argument-input" id="argument-value-{n}">
-</div>
+`
+<select class="argument-type" id="argument-type-{n}">
+    <option>None</option>
+    <option>Integer</option>
+    <option>Float</option>
+    <option>String</option>
+    <option>Function</option>
+</select>
+<input class="argument-input" id="argument-value-{n}">
 `;
 const ARGUMENT_CONTAINER      = document.getElementById("argument-container");
 const ARGUMENT_TITLE          = document.getElementById("argument-title");
@@ -25,8 +24,15 @@ let requiredArgumentCount = -1;
 
 function addArgument() {
     updateArgumentCount(argumentCount+1);
+
+    let div = document.createElement("div");
+    div.classList.add("argument");
+    div.id = `argument-${argumentCount}`;
+
     let html = ARGUMENT_TEMPLATE.replaceAll("{n}", argumentCount);
-    ARGUMENT_CONTAINER.innerHTML += html;
+    div.innerHTML = html;
+
+    ARGUMENT_CONTAINER.appendChild(div);
     REMOVE_ARGUMENT.disabled = false;
 }
 function removeArgument() {
