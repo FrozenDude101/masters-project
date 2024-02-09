6f0ae376c4c9b6e3e6a1c62f7ea701339ab401e2
Matthew Hammond <mxh162@student.bham.ac.uk>
2024-02-09 17:56:48 +0000

    Container experiment

--- src/scripts/interpreter/Thunk.js
+++ src/scripts/interpreter/Thunk.js
@@ -122,6 +122,7 @@ class ApplicationThunk {
     }
 
     constructor(t1, t2) {
+        this.id = Math.random(); // Probably unique.
         this.t1 = new ThunkWrapper(t1);
         this.t2 = new ThunkWrapper(t2);
     }
@@ -129,9 +130,19 @@ class ApplicationThunk {
         return new ApplicationThunk(this.t1.clone(), this.t2.clone());
     }
     toString() {
+        let content;
         if (this.t2.thunk instanceof ApplicationThunk)
-            return `${this.t1} (${this.t2})`;
-        return `${this.t1} ${this.t2}`;
+            content = `${this.t1} ${this.t2}`;
+        else
+            content = `${this.t1} ${this.t2}`;
+        return `
+            <span class="application"
+                id="${this.parent.id}"
+                onclick="applicationClick(event, ${this.id})"
+                onmouseover="applicationMouseOver(event, ${this.id})"
+                onmouseout="applicationMouseOut(event, ${this.id})"
+            >${content}</span>
+        `.trim();
     }
 
     canStep() {
--- src/scripts/ui/graph.js
+++ src/scripts/ui/graph.js
@@ -1,5 +1,75 @@
-const cytoscapeContainer = document.getElementById("ctyoscape-container");
+let cols = {
+    0: document.getElementById("cc0"),
+}
+let rows = {
+
+}
 
 function displayState() {
+    containers[0].innerHTML = `${state === null ? "" : state}`;
     cytoscapeContainer.innerHTML = `${state === null ? "" : state}`;
+}
+
+function applicationMouseOver(event, id) {
+    event.stopPropagation();
+    document.getElementById(id).classList.add("hover");
+}
+function applicationMouseOut(event, id) {
+    document.getElementById(id).classList.remove("hover");
+}
+function applicationClick(event, id) {
+    event.stopPropagation();
+}
+
+function createRow(id) {
+    let row = document.createElement("div");
+    row.classList.add("container-row");
+    row.id = `cr${id}`;
+    rows[id] = row;
+
+    cols[id].appendChild(row);
+    return row;
+}
+function createCol(id) {
+    let row = rows[id] ? rows[id] : createRow(id);
+
+    let n = 0;
+    while (cols[`${id}${n}`]) n++;
+
+    let col = document.createElement("div");
+    col.classList.add("container-column");
+    col.id = `cc${id}${n}`;
+    cols[`${id}${n}`] = col; 
+
+    let container = document.createElement("div");
+    container.classList.add("container");
+    container.id = `c${id}${n}`;
+    container.innerHTML = `
+        <button onclick="addContainer(event, '${id}${n}')">+</button>
+        <button onclick="removeContainer(event, '${id}${n}')">-</button>
+    `;
+
+    col.appendChild(container);
+    row.appendChild(col);
+    return col;
+}
+
+function addContainer(_, id) {
+    createCol(id);
+}
+function removeContainer(_, id) {
+    let n = 9;
+    while (!cols[`${id}${n}`] && n >= 0) n--;
+    if (n === -1) return;
+    for (let i = 0; i < 10; i++) {
+        removeContainer(null, `${id}${n}`)
+    }
+    let col = cols[`${id}${n}`]
+    let row = rows[id];
+    row.removeChild(col);
+    delete cols[`${id}${n}`];
+    if (n == 0) {
+        cols[`${id}`].removeChild(row);
+        delete rows[`${id}`]
+    }
 }
